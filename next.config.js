f73baacd6c973f7b83202ce406703dfb408dbcfc
Ian Walter <pub@ianwalter.dev>
2020-12-05 14:59:12 -0500

    Adding posts rewrite

--- next.config.js
+++ next.config.js
@@ -1,3 +1,17 @@
+const { promises: fs } = require('fs')
+const path = require('path')
 const withMDX = require('@next/mdx')({ extension: /\.mdx?$/ })
 
-module.exports = withMDX({ pageExtensions: ['js', 'jsx', 'md', 'mdx'] })
+function toRewrite (file) {
+  const filename = file.split('.')[0]
+  const slug = filename.split('~')[1]
+  return { source: `/${slug}`, destination: `/posts/${filename}`  }
+}
+
+module.exports = withMDX({
+  pageExtensions: ['js', 'jsx', 'md', 'mdx'],
+  async rewrites () {
+    const files = await fs.readdir(path.join(__dirname, 'pages/posts'))
+    return files.map(toRewrite)
+  }
+})
--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,5 @@
 import readme from '../README.md'
 
+export const layout = 'landing'
+
 export default readme
