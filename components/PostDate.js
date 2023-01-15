8144300dbef3dab1416a1a5a1188fa5de96bf55a
Ian Walter <pub@ianwalter.dev>
2023-01-15 15:16:40 -0500

    Fixing postdate component

--- components/PostDate.js
+++ components/PostDate.js
@@ -1,12 +1,13 @@
-import { format } from 'date-fns'
+const options = { dateStyle: 'long', timeZone: 'America/New_York' }
 
 export default function PostDate ({ date }) {
+  const estDate = new Date(`${date}T00:00:00-05:00`)
   return (
     <time
       className="text-zinc-400 text-[1rem]"
       dateTime={date}
     >
-      {format(new Date(date), 'MMMM d, yyyy')}
+      {estDate.toLocaleDateString('en-US', options)}
     </time>
   )
 }
