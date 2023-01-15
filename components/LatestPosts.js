b645f83fcafd319087861a61c55912f529dd46a7
Ian Walter <pub@ianwalter.dev>
2023-01-14 23:12:14 -0500

    Fixing body css

--- components/LatestPosts.js
+++ components/LatestPosts.js
@@ -4,15 +4,15 @@ import linkStyle from '../styles/linkStyle.js'
 
 export default function LatestPosts (props) {
   return (
-    <div css={{ marginTop: '2.5rem' }}>
+    <div className="mt-12">
 
-      <h2 className="text-3xl font-semibold" css={{ margin: 0, fontSize: '30px' }}>
+      <h2 className="text-3xl font-semibold">
         My latest posts 💬
       </h2>
 
       <ul className="pl-5 my-6 list-disc">
         {props.posts && props.posts.map(post => (
-          <li key={post.url}className="my-1">
+          <li key={post.url} className="my-1">
             <Link href={post.url} className={clsx(linkStyle, 'font-medium')}>
               {post.title}
             </Link>
