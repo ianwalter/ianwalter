c201146b214913c6920ffa033d66a26e74813fc9
Ian Walter <pub@ianwalter.dev>
2023-01-15 14:31:58 -0500

    Fixing posts logic

--- next.config.js
+++ next.config.js
@@ -1,22 +1,10 @@
-const { promises: fs } = require('fs')
-const path = require('path')
 const withMDX = require('@next/mdx')({
   extension: /\.mdx?$/,
   options: {
     rehypePlugins: [require('@mapbox/rehype-prism')]
   }
 })
-const deconstructFilename = require('./lib/deconstructFilename')
-
-function toRewrite (filename) {
-  const { slug } = deconstructFilename(filename)
-  return { source: `/${slug}`, destination: `/posts/${slug}` }
-}
 
 module.exports = withMDX({
   pageExtensions: ['js', 'jsx', 'md', 'mdx'],
-  async rewrites () {
-    const files = await fs.readdir(path.join(__dirname, 'pages/posts'))
-    return files.map(toRewrite)
-  }
 })
